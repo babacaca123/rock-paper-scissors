03c36d09118e7474991c5af5da75c2b77a327d47
ellis <elrestea@gmail.com>
2025-09-03 13:50:23 +0000

    add first computer function

---
+++ game-script.js
@@ -0,0 +1,23 @@
+console.log("hello world")
+
+humanSelection = ""
+computerSelection = ""
+
+function getComputerChoice() {
+    randomNum = Math.floor(Math.random() * 3);
+    if (randomNum === 0){
+        computerSelection = "rock";
+    }
+    else if (randomNum === 1){
+        computerSelection = "paper";
+    }
+    else if (randomNum === 2){
+        computerSelection = "scissors";
+    }
+    return computerSelection
+ 
+}
+
+function getHumanChoice(){
+    
+}
